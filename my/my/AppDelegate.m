56d6c65ea5f66a8b34bb8f411f0609c7ac02fc68
taiyang2014 <bml-ios@BMLiOSdeMac-mini.local>
2016-11-25 15:42:23 +0800

    注释信息

--- my/my/AppDelegate.m
+++ my/my/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //添加注释
     return YES;
 }
 
