11bdb1c65d363f170e9b33670f1ba99fe9ab078e
taiyang2014 <bml-ios@BMLiOSdeMac-mini.local>
2016-11-25 15:41:15 +0800

    add project

---
+++ my/my/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  my
+//
+//  Created by BML－iOS on 16/11/25.
+//  Copyright © 2016年 BML－iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ my/my/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  my
+//
+//  Created by BML－iOS on 16/11/25.
+//  Copyright © 2016年 BML－iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
